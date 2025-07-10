a281ebea1e405455488f712f5d9f9fce810b5ac4
Emon-Ahmed <technicalboyemon@gmail.com>
2025-07-11 03:59:57 +0600

    fix: step 1 page created

--- src/components/SelectPropertyType.tsx
+++ src/components/SelectPropertyType.tsx
@@ -1,42 +1,51 @@
 import React from 'react'
 import PropertyBox from './ui/PropertyBox'
 import rightIcon from "../assets/right-symbol.svg";
+import uploadIcon from "../assets/upload.svg";
 import Image from 'next/image'
 
 export default function SelectPropertyType() {
     return (
-        <div className='container mx-auto py-8'>
+        <div className='container mx-auto py-6'>
             <div className='py-1'>
                 <div className='text-2xl font-bold text-primary-text-black'>Property type</div>
-                <div className='my-6 flex-wrap grid grid-cols-3 gap-5'>
-                    <PropertyBox />
-                    <PropertyBox />
-                    <PropertyBox />
+                <div className='my-3 flex-wrap grid grid-cols-3 gap-5'>
+                    <PropertyBox active={false} />
+                    <PropertyBox active={false} />
+                    <PropertyBox active={true} />
                 </div>
             </div>
             <div className='py-1'>
                 <div className='text-2xl font-bold text-primary-text-black'>Select your role</div>
-                <div className='my-6 flex-wrap grid grid-cols-3 gap-5'>
-                    <PropertyBox />
-                    <PropertyBox />
-                    <PropertyBox />
+                <div className='my-3 flex-wrap grid grid-cols-3 gap-5'>
+                    <PropertyBox active={true} />
+                    <PropertyBox active={false} />
+                    <PropertyBox active={false} />
                 </div>
             </div>
-            <div className="py-1 ">
-                <div className='rounded-3xl border border-[#E8E8E8] my-2'>
-                    <div className='flex w-full items-center justify-between overflow-hidden rounded-tl-3xl rounded-tr-3xl border-b border-[#E8E8E8] bg-[#F4F4F4] px-6 py-4'>
-                        <p className='text-lg font-semibold text-[#272B35]'>Proof of ownership</p>
+            <div className="py-1">
+                <div className='rounded-xl border border-[#E8E8E8] my-2'>
+                    <div className='flex w-full items-center justify-between overflow-hidden rounded-tl-xl rounded-tr-xl border-b border-[#E8E8E8] bg-[#F4F4F4] px-6 py-3'>
+                        <p className='text-lg font-medium text-[#272B35]'>Proof of ownership</p>
                     </div>
-                    <div className='px-6 py-4'>
-                        <div className='flex flex-col gap-4'>
-                            <p className="text-lg font-semibold text-[#272B35]">Ownership doc*</p>
-                            <div className='flex items-center gap-2'>
-                                <Image src={rightIcon} alt='Home Icon' width={20} height={20} />
-                                <span>Accept RentYard property adding terms & condition</span>
+                    <div className='px-6 py-6'>
+                        <div className='flex flex-col gap-2'>
+                            <p className="text-[16px] font-medium text-[#272B35]">Ownership doc*</p>
+                            <div className="flex items-center">
+                                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-96 h-12 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-[#f6f6f6]  hover:bg-gray-100">
+                                    <div className="flex items-center justify-center gap-2">
+                                        <Image src={uploadIcon} alt='Home Icon' width={20} height={20} />
+                                        <p className="text-sm font-semibold text-gray-500 "><span className="font-semibold">(Pdf Only)</span></p>
+                                    </div>
+                                    <input id="dropzone-file" type="file" className="hidden" />
+                                </label>
                             </div>
                         </div>
                     </div>
-
+                </div>
+                <div className='flex items-center gap-2 my-4'>
+                    <Image src={rightIcon} alt='Home Icon' width={20} height={20} />
+                    <span>Accept RentYard property adding terms & condition</span>
                 </div>
             </div>
         </div>
--- src/components/ui/PropertyBox.tsx
+++ src/components/ui/PropertyBox.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
-import homeIcon from './../../assets/home.svg' // Adjust the path as necessary
+import homeIcon from './../../assets/home.svg'
 
-export default function PropertyBox() {
+export default function PropertyBox({ active }: { active: boolean }) {
     return (
-        <div className='border border-gray-300 rounded-xl p-6 flex'>
+        <div className={`border rounded-xl p-6 flex` + `${active ? ' bg-blue-light border-[#316EED]' : ' bg-white border-gray-300'}  cursor-pointer hover:bg-blue-light hover:border-[#316EED] transition-all duration-300`
+        }>
             <div className="bg-blue-light w-15 h-15 rounded-lg inline-flex items-center justify-center    ">
                 <Image src={homeIcon} alt='Home Icon' width={28} height={28} />
             </div>
@@ -12,6 +13,6 @@ export default function PropertyBox() {
                 <span className='text-primary-text-black'>Single House Property</span>
                 <span className='text-secondary-text-black'>Single unit house for single family</span>
             </div>
-        </div>
+        </div >
     )
 }
